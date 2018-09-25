10bc96646844221ece560f91b03d1b1479b9edfe
Phanuphat Cholatda <remix.tk108@gmail.com>
2018-09-25 17:12:31 +0700

    poc delete sheet

--- src/main/java/myx/poc/RemoveSheet.java
+++ src/main/java/myx/poc/RemoveSheet.java
@@ -3,7 +3,9 @@ package myx.poc;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -16,8 +18,10 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class ReadXSLS {
+public class RemoveSheet {
+
 	public static void main(String[] args) {
+
 		String fileName = "/Users/myx4play/Downloads/Template_Justify_New_Community_2018_Test_v3_Pico.xlsx";
 
 		try {
@@ -27,33 +31,22 @@ public class ReadXSLS {
 			System.out.println(workbook.getNumberOfSheets());
 
 			List<String> sheetName = new ArrayList<>();
-
+			HashMap<String, Integer> sheetNameMap = new HashMap<>();
 			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
 				sheetName.add(workbook.getSheetName(i));
-
+				sheetNameMap.put(workbook.getSheetName(i), i);
 				System.out.println("Sheet name: " + workbook.getSheetName(i));
 			}
 			System.out.println("sheet name :" + sheetName.contains("Details"));
 
-			Sheet datatypeSheet = workbook.getSheetAt(0);
-			Iterator<Row> rows = datatypeSheet.iterator();
+			workbook.removeSheetAt(sheetNameMap.get("Details").intValue());
 
-			while (rows.hasNext()) {
-				Row currentRow = rows.next();
-				Iterator<Cell> cells = currentRow.iterator();
-
-				while (cells.hasNext()) {
-					Cell currentCell = cells.next();
-
-					if (currentCell.getCellTypeEnum() == CellType.STRING) {
-						System.out.print(currentCell.getStringCellValue() + "--");
-					} else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
-						System.out.print(currentCell.getNumericCellValue() + "--");
-					}
-
-				}
-
+			for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+				System.out.println("Sheet name: " + workbook.getSheetName(i));
 			}
+
+			OutputStream file = new FileOutputStream("/Users/myx4play/Downloads/Template_Justify_New_Community_2018_Test_v3_Pico.xlsx");
+			workbook.write(file);
 
 		} catch (FileNotFoundException e) {
 
@@ -64,4 +57,5 @@ public class ReadXSLS {
 		}
 
 	}
+
 }
