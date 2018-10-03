583a343967ec438430e036fee18c91fa8ed6a887
Phanuphat Cholatda <remix.tk108@gmail.com>
2018-10-03 11:12:45 +0700

    poc add sheet

---
+++ src/main/java/myx/poc/AddSheet.java
@@ -0,0 +1,49 @@
+package myx.poc;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class AddSheet {
+
+	public static void main(String[] args) {
+		String fileName = "/Users/myx4play/Downloads/Template_Justify_New_Community_2018_Test_v3_Pico.xlsx";
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		XSSFSheet sheet = workbook.createSheet("Datatypes in Java");
+		Object[][] datatypes = { { "Datatype", "Type", "Size(in bytes)" }, { "int", "Primitive", 2 }, { "float", "Primitive", 4 }, { "double", "Primitive", 8 }, { "char", "Primitive", 1 },
+				{ "String", "Non-Primitive", "No fixed size" } };
+
+		int rowNum = 0;
+		System.out.println("Creating excel");
+
+		for (Object[] datatype : datatypes) {
+			Row row = sheet.createRow(rowNum++);
+			int colNum = 0;
+			for (Object field : datatype) {
+				Cell cell = row.createCell(colNum++);
+				if (field instanceof String) {
+					cell.setCellValue((String) field);
+				} else if (field instanceof Integer) {
+					cell.setCellValue((Integer) field);
+				}
+			}
+		}
+
+		try {
+			FileOutputStream outputStream = new FileOutputStream(fileName);
+			workbook.write(outputStream);
+			workbook.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Done");
+	}
+}
